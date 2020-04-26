f1e2b78209c3a3ed32cca4af015a5df7b2ba3512
SiminaFlorinaMuresan <Simina.Florina.Muresan@ibm.com>
2020-04-26 16:41:34 +0300

    spring boot skeleton project

---
+++ src/main/java/com/ibm/aidpad/AidpadApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.aidpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/aidpad/AidpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.aidpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
